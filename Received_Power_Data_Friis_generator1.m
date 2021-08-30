d79972af120f46166231f90e9534e43aa5a98b0c
anakings <61946570+anakings@users.noreply.github.com>
2021-08-31 00:40:34 +0200

    Add files via upload

---
+++ Received_Power_Data_Friis_generator1.m
@@ -0,0 +1,38 @@
+clear;clc;
+
+antenna_type = dipole;
+
+iterations = 500;
+ 
+frequency = 70e6;
+
+PtdBm = 10; % Tx power given in dBm
+
+folder = '/DOA_Data/antennas/';
+mkdir([pwd folder]); % Create folder
+
+% Create an array with random values of distance from transmitter to system
+rng('default');
+min_dist = 10;
+max_dist = 1000;
+p_matrix = zeros(iterations, 1);
+for i = 1:iterations
+    %p_matrix(i) = randi(100);
+    p_matrix(i) = (max_dist-min_dist)*rand() + min_dist;
+end
+
+for antennasNumber = 2:16 % Generate data for a two to 16 antenna system
+     mkdir([pwd folder int2str(antennasNumber) '/']);
+     name = strcat(pwd, folder, int2str(antennasNumber), '/iter_');
+     N = antennasNumber;
+     Received_Power_Data_Friis1(N, frequency, iterations, name, antenna_type, p_matrix, PtdBm);
+end
+
+%N = 4;
+%folder = '/DOA_Data/samples/';
+%mkdir([pwd folder]);
+%for samples = 100:100:500 %fix this
+%    mkdir([pwd folder int2str(samples) '/']);
+%    name = strcat(pwd, folder, int2str(samples), '/iter_');
+%    iterations = samples;
+%   Received_Power_Data_Friss(N, frequency, iterations, name, antenna_type);
